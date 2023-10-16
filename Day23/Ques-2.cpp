9926524917d622db5cdcb92297bcfa05748bd210
Ashfaq Khan <82702762+ashfaqrafin@users.noreply.github.com>
2023-10-16 13:26:25 +0600

    second question

---
+++ Day23/Ques-2.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <set>
+#include <string.h>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <climits>
+#include <math.h>
+#include <cmath>
+#include <cstdint>
+#include <bit>
+#include <bitset>
+#include <numeric>
+using namespace std;
+
+const int N = 200007;
+const int INF = 1e9;
+const int mod = 1e9 + 7;
+
+#define int long long
+#define double long double
+#define pb push_back
+#define all(container) container.begin(), container.end()
+// loop
+#define rep(variable, start, end) for (int variable = start; variable < end; variable++)
+#define repe(variable, start, end) for (int variable = start; variable <= end; variable++)
+#define repn(variable, start, end) for (int variable = start; variable >= end; variable--)
+#define repa(variable, container) for (auto &variable : container)
+// vector
+#define vi vector<int>
+#define vc vector<char>
+// pair
+#define pii pair<int, int>
+
+signed main(void)
+{
+
+    // cout.precision(10);
+    // cout.setf(ios::fixed);
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+
+    int row, col;
+    cin >> row >> col;
+
+    vector<vector<int>> given(row, vector<int>(col)), dp(row + 1, vector<int>(col + 1));
+    repa(i, given)
+    {
+        repa(j, i)
+        {
+            cin >> j;
+        }
+    }
+
+    repe(i, 1, row)
+    {
+        repe(j, 1, col)
+        {
+            if (given[i - 1][j - 1])
+            {
+                dp[i][j] = 1 + min(dp[i - 1][j], min(dp[i - 1][j - 1], dp[i][j - 1]));
+            }
+        }
+    }
+
+    repa(i, dp)
+    {
+        repa(j, i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
